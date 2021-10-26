ff9b1d24a4f1f512fdd170bfd8ffe1151fc15c51
zmeri <22378936+zmeri@users.noreply.github.com>
2021-10-26 08:46:44 +0300

    adding page on transient reactors

---
+++ examples/aku_kuumenemine.py
@@ -0,0 +1,96 @@
+'''
+Kui akud on ebastabiilses seisundis, ühendid akus võivad hakata reageerima, põhjustades tulekahju. Selline olukord võib näiteks tekkida, kui aku temperatuur on liiga kõrge või kui laaditakse liiga palju. On võimalik teha turvalisemaid akusid, kui on mudelid, mis kirjeldavad neid protsesse akus, mis võivad viia tulekahjuni.
+
+Oletame, et liitium-ioon aku pannakse keskkonda, mis on 240 kraadi juures. Kui kaua läheks enne, kui reaktsioonid aku sees põhjustavad järsku temperatuuri tõusu (ja tõenäoliselt tulekahju)? Vajalikud parameetrid antakse Hewsoni artiklis.
+
+Viited
+------
+* J. C. Hewson, “Understanding the limits of thermal runaway in lithium-ion battery systems,” Sandia National Lab. (SNL-NM), Albuquerque, NM (United States), SAND2016-6054C, Jun. 2016. Accessed: Oct. 21, 2021. [Online]. Available: https://www.osti.gov/biblio/1420927
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from gekko import GEKKO
+from scipy.optimize import minimize
+
+m = GEKKO()
+
+# Mudelis on neli reaktsiooni:
+# 1) Metastabiilse tahke-elektrolüüdi liidese (ingl solid-electrolyte interface, SEI) lagunemine
+# 2) Anoodi-elektrolüüdi reaktsioon
+# 3) Elektrolüüdi lagunemine
+# 4) CoO2-elektrolüüdi reaktsioon
+
+# parameetrid artiklist
+Yalg_väärtused = np.asarray([0.15, 0.45, 1.0, 0.96]) # Y on sisuliselt normaliseeritud kontsentratsioon, ehk näitab, kui palju antud ainest on järgi jäänud
+A_väärtused = np.asarray([1.67e15, 1.67e6, 5.1e25, 6.67e11]) # s^-1, sagedustegur
+Ea_väärtused = np.asarray([135, 77.2, 274, 122]) * 1000 # J mol^-1, aktivatsioonienergia
+Hrxn_väärtused = np.asarray([257.0, 1714, 155, 314]) * 1000 # J kg^-1, reaktsioonientalpia
+rho_väärtused = np.asarray([500, 610.0, 1221, 407]) # kg m^-3, tihedus
+
+ncomp = 4 # reaktsioonide arv
+Yalg = m.Array(m.Param, (ncomp))
+A = m.Array(m.Param, (ncomp))
+Ea = m.Array(m.Param, (ncomp))
+Hrxn = m.Array(m.Param, (ncomp))
+rho = m.Array(m.Param, (ncomp))
+for i in range(ncomp):
+    Yalg[i].value = Yalg_väärtused[i]
+    A[i].value = A_väärtused[i]
+    Ea[i].value = Ea_väärtused[i]
+    Hrxn[i].value = Hrxn_väärtused[i]
+    rho[i].value = rho_väärtused[i]
+
+RGAS = m.Const(value=8.314462618) # J mol^-1 K^-1
+rhoCp = m.Param(value=2.79e6) # J m^-3
+pind_maht_suhe = m.Param(48.4) # m^-1
+epsilon = m.Param(0.8)
+sigma = m.Param(5.67e-8) # W m^-2 K^-4
+lm = 0.034 # W m^-1 K^-1
+d = 0.018 # m
+Nu = 4.8
+hc = Nu * lm / d # W m^-2 K^-1
+hbl = 7.17 # W m^-2 K^-1, soojusläbikandetegur piirkihi jaoks
+heff = m.Param(value = 1 / (1 / hc + 1 / hbl)) # soojusläbikandetegur
+
+npts = 1000
+t_lõpp = 3400
+m.time = np.linspace(0, t_lõpp, npts)
+Tvälis = m.Param(value=240)
+
+T = m.Var()
+T.value = 25 * np.ones(npts)
+Y0 = m.Var()
+Y0.value = Yalg[0].value * np.ones(npts)
+Y1 = m.Var()
+Y1.value = Yalg[1].value * np.ones(npts)
+Y2 = m.Var()
+Y2.value = Yalg[2].value * np.ones(npts)
+Y3 = m.Var()
+Y3.value = Yalg[3].value * np.ones(npts)
+
+z = m.Intermediate(0.033 + (Yalg[1] - Y1) + (Yalg[0] - Y0))
+k = [None] * ncomp
+for i in range(ncomp):
+    k[i] = m.Intermediate(A[i] * m.exp(-Ea[i] / RGAS / (T + 273.15)))
+
+Qrxn0 = m.Intermediate(k[0] * Y0 * rho[0] * Hrxn[0])
+Qrxn1 = m.Intermediate(k[1] * Y1 * m.exp(-z/0.033) * rho[1] * Hrxn[1])
+Qrxn2 = m.Intermediate(k[2] * Y2 * rho[2] * Hrxn[2])
+Qrxn3 = m.Intermediate(k[3] * Y3 * (1 - Y3) * rho[3] * Hrxn[3])
+Qkonv = m.Intermediate(heff * pind_maht_suhe * (Tvälis - T))
+Qkiirg = m.Intermediate(epsilon * sigma * pind_maht_suhe * ((Tvälis + 273.15)**4 - (T + 273.15)**4))
+m.Equation(T.dt() == (Qrxn0 + Qrxn1 + Qrxn2 + Qrxn3 + Qkonv + Qkiirg) / rhoCp)
+m.Equation(Y0.dt() == -k[0] * Y0)
+m.Equation(Y1.dt() == -k[1] * Y1 * m.exp(-z/0.033))
+m.Equation(Y2.dt() == -k[2] * Y2)
+m.Equation(Y3.dt() == -k[3] * Y3 * (1 - Y3))
+
+m.options.IMODE = 4
+m.solve(disp=False)
+
+plt.figure()
+plt.plot(m.time, T.value)
+plt.xlabel('Aeg (s)')
+plt.ylabel("Temperatuur (°C)")
+plt.savefig('aku-kuumenemine.png')
+plt.show()
---
+++ examples/metyylbromiid.py
@@ -0,0 +1,70 @@
+'''
+Metüülbromiidi  saamiseks  viiakse  läbi  pöördumatu  vedelfaasiline
+lihtreaktsioon poolperioodilises reaktoris:
+    CNBr + CH3NH2 ---> CH3Br + NCNH2
+    ehk  A  +  B ----> C  +  D
+Alguses on juba reaktoris 5 liitrit lahust, mis sisaldab 0,05 mol/l ainet
+A. Siis hakatakse lisama lahust, mis sisaldab aine B kontsentratsioonis
+0,025 mol/l. Leida  broomtsüaniidi  konversiooniastme sõltuvus ajast.
+Samuti leida broomtsüaniidi, metüülamiini  ja  metüülbromiidi
+kontsentratsiooni sõltuvus ajast.
+
+Teist lahust lisatakse mahtkuluga 0,05 l s^-1. Reaktsiooni kiiruskonstant
+antud tingimustel on 2,2 l s^-1 mol^-1.
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from gekko import GEKKO
+
+m = GEKKO()
+
+CA0 = m.Param(value=0.05) # mol/L
+CB0 = m.Param(value=0.025) # mol/L
+V0 = m.Param(value=5) # L
+k = m.Param(value=2.2) # L/s/mol
+v_in = 0.05 # L/s
+nA0 = m.Param(value=CA0.value * V0.value)
+
+npts = 1000
+t_lõpp = 400
+m.time = np.linspace(0, t_lõpp, npts) # s
+
+V = m.Param(value=V0.value + v_in * np.asarray(m.time))
+FB0 = m.Param(value=CB0.value * v_in * np.asarray(m.time))
+
+XA = m.Var(0)
+
+'''
+Diferentsiaalvõrrandi tuletamine:
+1) Alustada moolbilansiga: V * dCA_dt = r_A * V
+    - siin eeldame, et reaktsiooni kiirus on igal pool sama (ehk ei ole vaja integraali mahu suhtes)
+2) dCA_dt = -CA0 * dXA_dt
+3) Lisada reaktsioonikiiruse võrrandit
+4) Lahendada dXA_dt jaoks
+'''
+m.Equation(XA.dt() == k * (1 - XA) * (FB0 - nA0 * XA) / V)
+
+m.options.IMODE = 4
+m.solve(disp=False)
+
+CB = (FB0.value - nA0.value * np.asarray(XA.value)) / np.asarray(V.value)
+CC = (nA0.value * np.asarray(XA.value)) / np.asarray(V.value)
+CA = nA0.value * (1 - np.asarray(XA.value)) / np.asarray(V.value)
+
+plt.figure()
+plt.plot(m.time, XA.value)
+plt.xlabel('Aeg (s)')
+plt.ylabel('X$_A$')
+plt.savefig('../media/metyylbromiid_XA.png')
+plt.show()
+
+plt.figure()
+plt.plot(m.time, CA, label='CA')
+plt.plot(m.time, CB, label='CB')
+plt.plot(m.time, CC, label='CC')
+plt.xlabel('Aeg (s)')
+plt.ylabel('Kontsentratsioon (mol L$^{-1}$)')
+plt.legend(frameon=False)
+plt.savefig('../media/metyylbromiid_kontsentratsioonid.png')
+plt.show()
