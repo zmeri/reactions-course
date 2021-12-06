1eb69da6e70c18c9d1ba525f70a73b3ae358e544
zmeri <22378936+zmeri@users.noreply.github.com>
2021-12-06 12:11:58 +0200

    update enzyme kinetics

--- examples/maks.py
+++ examples/maks.py
@@ -6,7 +6,7 @@ Etanooli muundatakse maksas järgnevate reaktsioonide kaudu:
     CH3CHO + NAD+ -> CH3COOH + NADH + H+
 Ensüümid kiirendavad neid reaktsioone. Esimeses reaktsioonis alkoholdehüdrogenaas muudab etanooli atsetaldehüüdiks ja teises reaktsioonis aldehüüddehüdrogenaase abil atsetaldehüüd muutub atsetaadiks.
 
-Tee mudelit sellest, kuidas etanooli kontsentratsioon muutub peale seda, kui juuakse alkohoolset jooki. Kui juuakse 400 ml veini (etanooli sisaldus 12 vol%), kas etanooli kontsentratsioon veres ületaks Eesti alkoholi piirmäära (0,2 promilli, ehk 0,2 mg/g)? Kui ületab, kui kaua läheks enne, kui etanooli kontsentratsioon veres on jälle madalam, kui piirmäär?
+Tee mudelit sellest, kuidas etanooli kontsentratsioon muutub peale seda, kui juuakse alkohoolset jooki. Kui juuakse 120 ml veini (etanooli sisaldus 12 vol%), kas etanooli kontsentratsioon veres ületaks Eesti alkoholi piirmäära (0,2 promilli, ehk 0,2 mg/g)? Kui ületab, kui kaua läheks enne, kui etanooli kontsentratsioon veres on jälle madalam, kui piirmäär?
 '''
 import pandas as pd
 import numpy as np
@@ -16,7 +16,7 @@ from gekko import GEKKO
 m = GEKKO()
 
 # lähteandmed
-V_jook = 400 # ml, joogi maht
+V_jook = 120 # ml, joogi maht
 mahtosa_etanool = 0.12
 etanooli_tihedus = 0.78954976 # g/ml
 m_etanool_sisse = V_jook * mahtosa_etanool * etanooli_tihedus
@@ -25,7 +25,8 @@ maksu_mass = 1.5 # kg, Allikad: C. Eipel, K. Abshagen, and B. Vollmar, “Regula
 mm = 46.07 # etanooli molaar mass (g/mol)
 vere_tihedus = 1.0506 # g/ml, Allikas: R. J. Trudnowski and R. C. Rico, “Specific Gravity of Blood and Plasma at 4 and 37 °C,” Clinical Chemistry, vol. 20, no. 5, pp. 615–616, May 1974, doi: 10.1093/clinchem/20.5.615.
 maksu_tihedus = 1.051 # g/ml, Allikas: B. A. Overmoyer, C. E. McLaren, and G. M. Brittenham, “Uniformity of liver density and nonheme (storage) iron distribution,” Arch Pathol Lab Med, vol. 111, no. 6, pp. 549–554, Jun. 1987.
-Vmax = m.Param(value=10) # min^-1, Allikas: F. W. Wagner, A. R. Burger, and B. L. Vallee, “Kinetic properties of human liver alcohol dehydrogenase: oxidation of alcohols by class I isoenzymes,” Biochemistry, vol. 22, no. 8, pp. 1857–1863, Apr. 1983, doi: 10.1021/bi00277a018.
+kcat = m.Param(value=10) # min^-1, Allikas: F. W. Wagner, A. R. Burger, and B. L. Vallee, “Kinetic properties of human liver alcohol dehydrogenase: oxidation of alcohols by class I isoenzymes,” Biochemistry, vol. 22, no. 8, pp. 1857–1863, Apr. 1983, doi: 10.1021/bi00277a018.
+C_ensüüm = m.Param(value=2e-4) # mol/l, põhineb andmetele artiklist T. K. Li, W. F. Bosron, W. P. Dafeldecker, L. G. Lange, and B. L. Vallee, “Isolation of pi-alcohol dehydrogenase of human liver: is it a determinant of alcoholism?,” Proc Natl Acad Sci U S A, vol. 74, no. 10, pp. 4378–4381, Oct. 1977, doi: 10.1073/pnas.74.10.4378.
 Km = m.Param(value=1.2/1000) # M, Allikas: F. W. Wagner, A. R. Burger, and B. L. Vallee, “Kinetic properties of human liver alcohol dehydrogenase: oxidation of alcohols by class I isoenzymes,” Biochemistry, vol. 22, no. 8, pp. 1857–1863, Apr. 1983, doi: 10.1021/bi00277a018.
 V_maks = m.Param(value=maksu_mass / maksu_tihedus) # l
 V_veri = m.Param(value=5) # l, Allikas: J. Feldschuh and Y. Enson, “Prediction of the normal blood volume. Relation of blood volume to body habitus.,” Circulation, vol. 56, no. 4, pp. 605–612, Oct. 1977, doi: 10.1161/01.CIR.56.4.605.
@@ -33,8 +34,8 @@ V_magu = m.Param(value=0.5 + V_jook/1000) # l, mao maht
 V_sool = m.Param(value=0.06) # l, soolestiku maht
 vdot_magu_sool = m.Param(value=0.01) # l/min, mahtkulu maost soolestikku
 vdot_maks_veri = m.Param(value=maksu_mass) # l/min, vere mahtkulu maksasse on umbes 1 l / kg maksa. Allikas: C. Eipel, K. Abshagen, and B. Vollmar, “Regulation of hepatic blood flow: The hepatic arterial buffer response revisited,” World Journal of Gastroenterology : WJG, vol. 16, no. 48, p. 6046, Dec. 2010, doi: 10.3748/wjg.v16.i48.6046.
-h_magu = m.Param(value=0.0005) # min^-1
-h_sool = m.Param(value=0.05) # min^-1
+h_magu = m.Param(value=0.001) # min^-1
+h_sool = m.Param(value=0.5) # min^-1
 
 Cet_magu_alg = m_etanool_sisse / mm / V_magu.value # arvutada etanooli algkontsentratsioon kõhus
 
@@ -49,7 +50,7 @@ Cet_veri = m.Var(0) # mol/l, etanooli kontsentratsioon veres
 Cet_maks = m.Var(0) # mol/l, etanooli kontsentratsioon maksas
 
 # mudeli võrrandid
-r = m.Intermediate(-Vmax * Cet_maks / (Km + Cet_maks))
+r = m.Intermediate(-kcat * C_ensüüm * Cet_maks / (Km + Cet_maks))
 ndot_maost = m.Intermediate(h_magu * (Cet_magu - Cet_veri)) # mol/min
 ndot_sool = m.Intermediate(h_sool * (Cet_sool - Cet_veri)) # mol/min
 m.Equation(Cet_magu.dt() == (-ndot_maost - vdot_magu_sool * Cet_magu) / V_magu) # tegelikult mao maht pidevalt muutub, aga siin lihtsustame ja oletame, et maht ei muutu.
